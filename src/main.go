98dd2a5bd6394ced2f2ab06e80be56e9a8aa1c18
femsworld <femsworld@gmail.com>
2024-06-02 23:35:00 +0300

    Added check for too many arguments

--- src/main.go
+++ src/main.go
@@ -1,61 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
-    "stations/src/functions"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"stations/src/functions"
 )
 
-func printUsage() {
-    fmt.Fprintln(os.Stderr, "Usage: go run . [path to file containing network map] [start station] [end station] [number of trains]")
-}
-
 func main() {
-    // Check if the correct number of command-line arguments is provided
-    if len(os.Args) < 5 {
-        fmt.Fprintf(os.Stderr, "Error: Too few command line arguments\n")
-        printUsage()
-        os.Exit(1)
-    }
+	if len(os.Args) < 4 || len(os.Args) > 5 {
+		fmt.Println("Usage: go run main.go <map_file> <start_station> <end_station> [num_trains]")
+		os.Exit(1)
+	}
 
-    // Extract command-line arguments
-    networkMapPath := os.Args[1]
-    startStation := os.Args[2]
-    endStation := os.Args[3]
-    numTrains, err := strconv.Atoi(os.Args[4])
-    if err != nil || numTrains <= 0 {
-        fmt.Fprintln(os.Stderr, "Error: Number of trains is not a valid positive integer")
-        printUsage()
-        os.Exit(1)
-    }
+	mapFile := os.Args[1]
+	startStation := strings.ToLower(os.Args[2])
+	endStation := strings.ToLower(os.Args[3])
 
-    graph, err := functions.ReadGraphFromFile(networkMapPath)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error reading network map: %v\n", err)
-        os.Exit(1)
-    }
+	numTrains := 3 // default number of trains
+	if len(os.Args) == 5 {
+		var err error
+		numTrains, err = strconv.Atoi(os.Args[4])
+		if err != nil {
+			fmt.Println("Error: num_trains should be a valid integer.")
+			os.Exit(1)
+		}
+		if numTrains <= 0 {
+			fmt.Println("Error: Number of trains should be greater than 0.")
+			os.Exit(1)
+		}
+	}
 
-    if !graph.IsValidStation(startStation) {
-        fmt.Fprintln(os.Stderr, "Error: Start station does not exist")
-        os.Exit(1)
-    }
+	graph, err := functions.ReadGraphFromFile(mapFile)
+	if err != nil {
+		fmt.Printf("Error reading graph from file: %v\n", err)
+		os.Exit(1)
+	}
 
-    if !graph.IsValidStation(endStation) {
-        fmt.Fprintln(os.Stderr, "Error: End station does not exist")
-        os.Exit(1)
-    }
+	if !graph.IsValidStation(startStation) {
+		fmt.Printf("Error: Start station %s does not exist in the map.\n", startStation)
+		os.Exit(1)
+	}
+	if !graph.IsValidStation(endStation) {
+		fmt.Printf("Error: End station %s does not exist in the map.\n", endStation)
+		os.Exit(1)
+	}
 
-    if startStation == endStation {
-        fmt.Fprintln(os.Stderr, "Error: Start and end station are the same")
-        os.Exit(1)
-    }
+	if !graph.PathExists(startStation, endStation) {
+		fmt.Printf("Error: No path exists between %s and %s.\n", startStation, endStation)
+		os.Exit(1)
+	}
 
-    if !graph.PathExists(startStation, endStation) {
-        fmt.Fprintln(os.Stderr, "Error: No path between the start and end stations")
-        os.Exit(1)
-    }
-
-    fmt.Println("Scheduling trains...")
-    functions.ScheduleTrains(graph, startStation, endStation, numTrains)
+	functions.ScheduleTrains(graph, startStation, endStation, numTrains)
 }
